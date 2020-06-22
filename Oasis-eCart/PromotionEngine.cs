ab175cf531b516bdf30feac11ed747a9aac9eaed
saday-sarkar <saday.sarkar@outlook.com>
2020-06-22 16:18:43 +0530

    Pass all test cases

--- Oasis-eCart/PromotionEngine.cs
+++ Oasis-eCart/PromotionEngine.cs
@@ -24,6 +24,27 @@ namespace Oasis_eCart
         private int ApplyPromotions(MyCart myCart, IEnumerable<StockKeepingUnit> RepositoryItems, IEnumerable<Promotion> Promotions)
         {
             int Result = 0;
+            //Promotion Type 1 (Promo 1 and 2)
+            foreach (var promo in Promotions.Where(y => y.Type == 1).ToList())
+            {
+                while (myCart.MyCartItems.Where(x => x.MyCartItemID == promo.SKUID1).FirstOrDefault()?.MyCartItemUnit >= promo.SKUUnit)
+                {
+                    Result += promo.Price;
+                    myCart.MyCartItems.Where(x => x.MyCartItemID == promo.SKUID1).FirstOrDefault().MyCartItemUnit -= promo.SKUUnit;
+                }
+            }
+
+            //Promotion Type 2 ( Promotion 3 )
+            foreach (var promo in Promotions.Where(y => y.Type == 2).ToList())
+            {
+                while (myCart.MyCartItems.Where(x => x.MyCartItemID == promo.SKUID1).FirstOrDefault()?.MyCartItemUnit >= 1 && myCart.MyCartItems.Where(x => x.MyCartItemID == promo.SKUID2).FirstOrDefault()?.MyCartItemUnit >= 1)
+                {
+                    Result += promo.Price;
+                    myCart.MyCartItems.Where(x => x.MyCartItemID == promo.SKUID1).FirstOrDefault().MyCartItemUnit -= 1;
+                    myCart.MyCartItems.Where(x => x.MyCartItemID == promo.SKUID2).FirstOrDefault().MyCartItemUnit -= 1;
+                }
+            }
+
             //With No Promotion
             while (myCart.MyCartItems.Count() > 0)
             {
