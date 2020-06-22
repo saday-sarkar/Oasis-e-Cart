269e08fe83981b2727d02c71dce42803345e194c
saday-sarkar <saday.sarkar@outlook.com>
2020-06-22 15:45:02 +0530

    Initial Solutions

---
+++ Oasis-eCart/PromotionEngineTest.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oasis_eCart
+{
+    [TestFixture]
+    class PromotionEngineTest
+    {
+        [TestCase]
+        public void CheckoutCartTestScenarioA()
+        {
+            MyCart myCart = new MyCart
+            {
+                MyCartItems = new List<MyCartItem>()
+                {
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Apple",
+                        MyCartItemID = 'A',
+                        MyCartItemUnit = 1
+                    },
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Banana",
+                        MyCartItemID = 'B',
+                        MyCartItemUnit = 1
+                    },
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Cherrie",
+                        MyCartItemID = 'C',
+                        MyCartItemUnit = 1
+                    }
+                }
+            };
+            Assert.AreEqual(PromotionEngine.CheckOutCart(myCart), 100);
+
+        }
+        [TestCase]
+        public void CheckoutCartTestScenarioB()
+        {
+            MyCart myCart = new MyCart
+            {
+                MyCartItems = new List<MyCartItem>()
+                {
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Apple",
+                        MyCartItemID = 'A',
+                        MyCartItemUnit = 5
+                    },
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Banana",
+                        MyCartItemID = 'B',
+                        MyCartItemUnit = 5
+                    },
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Cherrie",
+                        MyCartItemID = 'C',
+                        MyCartItemUnit = 1
+                    }
+                }
+            };
+            Assert.AreEqual(PromotionEngine.CheckOutCart(myCart), 370);
+
+        }
+        [TestCase]
+        public void CheckoutCartTestScenarioC()
+        {
+            MyCart myCart = new MyCart
+            {
+                MyCartItems = new List<MyCartItem>()
+                {
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Apple",
+                        MyCartItemID = 'A',
+                        MyCartItemUnit = 3
+                    },
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Banana",
+                        MyCartItemID = 'B',
+                        MyCartItemUnit = 5
+                    },
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Cherrie",
+                        MyCartItemID = 'C',
+                        MyCartItemUnit = 1
+                    },
+                    new MyCartItem
+                    {
+                        MyCartItemName = "Damson",
+                        MyCartItemID = 'D',
+                        MyCartItemUnit = 1
+                    }
+                }
+            };
+            Assert.AreEqual(PromotionEngine.CheckOutCart(myCart), 280);
+        }
+    }
+}
---
+++ Oasis-eCart/UtilityModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oasis_eCart
+{
+    public class MyCart
+    {
+        public List<MyCartItem> MyCartItems { get; set; }
+    }
+
+    public class MyCartItem
+    {
+        public char MyCartItemID { get; set; }
+        public string MyCartItemName { get; set; }
+        public int MyCartItemUnit { get; set; }
+    }
+}
