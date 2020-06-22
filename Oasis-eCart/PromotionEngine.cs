fea22836c0c968a6127c598af5501164d08469d4
saday-sarkar <saday.sarkar@outlook.com>
2020-06-22 16:12:24 +0530

    Unit Test Case passed with this changes

--- Oasis-eCart/PromotionEngine.cs
+++ Oasis-eCart/PromotionEngine.cs
@@ -19,7 +19,19 @@ namespace Oasis_eCart
         {
             var RepositoryItems = this.SKURepoService.GetStockKeepingUnits();
             var Promotions = this.promotionRepositoryService.GetPromotions();
-            return 0;
+            return ApplyPromotions(myCart, RepositoryItems, Promotions);
+        }
+        private int ApplyPromotions(MyCart myCart, IEnumerable<StockKeepingUnit> RepositoryItems, IEnumerable<Promotion> Promotions)
+        {
+            int Result = 0;
+            //With No Promotion
+            while (myCart.MyCartItems.Count() > 0)
+            {
+                var item = myCart.MyCartItems.FirstOrDefault();
+                Result += (RepositoryItems.Where(z => z.SKUID == item.MyCartItemID).FirstOrDefault().SKUUnitPrice * item.MyCartItemUnit);
+                myCart.MyCartItems.Remove(item);
+            }
+            return Result;
         }
     }
 }
